61815f982ad0ea1ae537c8731674b91f54bbe8db
SanchoPansa <zelgadis4@gmail.com>
2026-06-18 23:28:51 +0300

    Moved l8n objects to separate files

---
+++ web/src/components/i18n/en.ts
@@ -0,0 +1,26 @@
+export default {
+  language: {
+    self: "Language"
+  },
+  navigation: {
+    planner: "Planner",
+    graph: "Graph (WIP)",
+    recipes: "Recipes",
+    changelog: "Change Log"
+  },
+  tabnavigation: {
+    confirm: "Are you sure you wish to delete this tab? This action is irreversible!"
+  },
+  statistics: {
+    header: "Statistics [WIP]",
+    hide: "Hide",
+    show: "Show",
+    hideAll: "Hide all Products",
+    showAll: "Show all Protucts",
+    resources: {
+      header: "Raw Resources",
+      info: "Shows the amount of raw resources consumed by all your factories.",
+      noResources: "Awaiting Resource Consumption"
+    }
+  }
+}
--- web/src/main.ts
+++ web/src/main.ts
@@ -16,6 +16,8 @@ import '@/assets/styles/global.scss'
 import { inject } from '@vercel/analytics'
 import { injectSpeedInsights } from '@vercel/speed-insights'
 import { createI18n } from 'vue-i18n'
+import en from './components/i18n/en.js'
+import ru from './components/i18n/ru.js'
 
 inject()
 injectSpeedInsights()
@@ -27,22 +29,8 @@ const i18n = createI18n( {
   locale: "ru",
   fallbackLocale: "en",
   messages: {
-    en: {
-      navigation: {
-        planner: "Planner",
-        graph: "Graph (WIP)",
-        recipes: "Recipes",
-        changelog: "Change Log"
-      }
-    },
-    ru: {
-      navigation: {
-        planner: "Планировщик",
-        graph: "Схема",
-        recipes: "Рецепты",
-        changelog: "История изменений"
-      }
-    }
+    en: en,
+    ru: ru
   }
 });
 
