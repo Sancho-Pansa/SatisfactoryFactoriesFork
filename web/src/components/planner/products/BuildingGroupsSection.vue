7e0d78e54515022a2afdc059e5a67b6aebc397c4
SanchoMint <zelgadis4@gmail.com>
2026-07-24 18:20:03 +0300

    i18n: BuildingSgroupsSection.vue (WIP)

--- web/src/components/planner/products/BuildingGroupsSection.vue
+++ web/src/components/planner/products/BuildingGroupsSection.vue
@@ -15,14 +15,20 @@
       </span>
       <i class="fas fa-layer-group" />
       <span v-if="item.buildingGroupsHaveProblem" class="ml-2">
-        <i class="fas fa-exclamation-triangle" /> Building Groups have a problem!
+        <i class="fas fa-exclamation-triangle" /> {{ $t('planner.factory.productsAndPower.buildingGroups.problem') }}
       </span>
       <span v-else class="ml-2">
-        {{ item.buildingGroupsTrayOpen ? 'Close' : 'Open' }} Building Groups ({{ item.buildingGroups.length }})
+        {{ $t(
+          'planner.factory.productsAndPower.buildingGroups.header',
+          {
+            action: item.buildingGroupsTrayOpen ? $t('planner.factory.productsAndPower.buildingGroups.headerClose') : $t('planner.factory.productsAndPower.buildingGroups.headerOpen'),
+            num: item.buildingGroups.length
+          })
+        }}
         <tooltip-info :is-caption="false" :text="introTooltip" />
       </span>
       <span class="ml-3 d-inline-flex align-center">
-        <tooltip :text="`Total Power Shards needed by this ${itemNoun}'s Building Groups (1 per building per 50% clock above 100%)`">
+        <tooltip :text="$t('planner.factory.productsAndPower.buildingGroups.powerShardsTooltip', {noun: itemNoun})">
           <game-asset height="18px" subject="power-shard" type="item_id" width="18px" />
         </tooltip>
         <span :id="`${idPrefix}-power-shards-total`" class="ml-1">{{ getTotalPowerShards(item.buildingGroups) }}</span>
@@ -50,6 +56,7 @@
   import { Factory, FactoryItem, FactoryPowerProducer, ItemType } from '@/interfaces/planner/FactoryInterface'
   import { getTotalPowerShards, toggleBuildingGroupTray } from '@/utils/factory-management/building-groups/common'
   import { getSomersloopBuildCost, getTotalSomersloops } from '@/utils/factory-management/building-groups/somersloops'
+  import { useI18n } from 'vue-i18n'
 
   const props = defineProps<{
     factory: Factory
@@ -60,17 +67,19 @@
     disabled?: boolean
   }>()
 
-  const itemNoun = computed(() => props.type === ItemType.Product ? 'product' : 'producer')
+  const { t } = useI18n()
+
+  const itemNoun = computed(() => props.type === ItemType.Product ? t('planner.factory.productsAndPower.buildingGroups.tooltipNounProduct') : t('planner.factory.productsAndPower.buildingGroups.tooltipNounProducer'))
 
   const somersloopBuildCost = computed(() => getSomersloopBuildCost(props.building))
 
   const somersloopTooltip = computed(() => somersloopBuildCost.value > 0
-    ? `Total Somersloops consumed constructing this ${itemNoun.value}'s buildings (${somersloopBuildCost.value} each)`
-    : "Total Somersloops used by this product's Building Groups",
+    ? t('planner.factory.productsAndPower.buildingGroups.somersloopsToBuild', { num: somersloopBuildCost.value })
+    : t('planner.factory.productsAndPower.buildingGroups.somersloopsTotal'),
   )
 
   const introTooltip = computed(() => props.type === ItemType.Product
-    ? "Building Groups turn this product's abstract building count into the real machines you'd build in-game: sets of identical buildings, each group with its own building count, overclock % and Somersloops.<br>Use them to plan your exact layout and see true per-group part rates and power usage."
-    : "Building Groups turn this producer's abstract building count into the real generators you'd build in-game: sets of identical buildings, each group with its own building count and overclock %.<br>Use them to plan your exact layout and see true per-group fuel rates and power output.",
+    ? t('planner.factory.productsAndPower.buildingGroups.productIntroTooltip')
+    : t('planner.factory.productsAndPower.buildingGroups.producerIntroTooltip'),
   )
 </script>
